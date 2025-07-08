694c7669caee5c387ee0435ac29fcd37bff64c4e
srisaagar <143073486+srisaagar@users.noreply.github.com>
2025-07-08 07:24:20 +0000

    nodejs

---
+++ Assignment9/main.js
@@ -0,0 +1,5 @@
+// main.js
+import { add as sum, subtract as diff } from './mathOperations.js';
+
+console.log("Sum of 5 and 3:", sum(5, 3));         // Output: 8
+console.log("Difference of 5 and 3:", diff(5, 3)); // Output: 2
---
+++ Assignment9/mathOperations.js
@@ -0,0 +1,8 @@
+// mathOperations.js
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
